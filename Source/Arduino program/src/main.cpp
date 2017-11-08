99fea606769ca7cb0c10e3a27d98ea7c21c97575
OpltFrantisek <opltfrantisek@gmail.com>
2017-11-08 20:05:24 +0100

    pridani nastaveni casu

--- Source/Arduino program/src/main.cpp
+++ Source/Arduino program/src/main.cpp
@@ -7,23 +7,39 @@
 #include <Time.h>
 #include "DS3231.h"
 
-int INTERRUPT_PIN = 2; 
-int h = 1;
-int m = 12;
-int s = 24;
+#include <Keypad.h>
+//keypad
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+
+//-----------------------------------------------------------------------
+
+int INTERRUPT_PIN = 10; 
+int h;
+int m;
+int s;
 int t = 4563;
-bool h_flag = false;
-bool m_flag = false;
-bool s_flag = false;
 
 bool boom = false;
 
-char cas[] = {'0','1',':','1','2',':','2','6'};
+//char cas[] = {'0','1',':','1','2',':','2','6'};
 
 LiquidCrystal_I2C lcd(0x3F,16,2); 
 
 bool alarm;
-int count = 255;
 DS3231 clock;
 
 
@@ -58,37 +74,72 @@ void AlarmInterrupt(){
     pocitej();
   }  
 }
+int NastavCas(){
+  int index=0;
+  int cisla[8] = {0,0,-1,0,0,-1,0,0};
+  lcd.setCursor(index, 0);
+   lcd.print("Nastav cas:"); // vypĂ­Ĺˇe text
+   lcd.setCursor(index, 1);
+    lcd.print("00:00:00");
+    lcd.setCursor(index, 1);
+    lcd.cursor_on();
+    while(true){
+      char customKey = customKeypad.getKey();
+      switch(customKey){
+        case 'D': break;
+        case '*': if(index > 0){
+                        index--;
+                        lcd.setCursor(index, 1);
+                      }
+                  break;
+        case '#': if(index < 15){
+                        index++;
+                        lcd.setCursor(index, 1);
+                      }
+                  break;
+      case '0': if(index != 2 && index != 5){cisla[index] = 0;lcd.print(0);}index++;lcd.setCursor(index,1);break;
+      case '1': if(index != 2 && index != 5){cisla[index] = 1;lcd.print(1);}index++;lcd.setCursor(index,1);break;
+      case '2': if(index != 2 && index != 5){cisla[index] = 2;lcd.print(2);}index++;lcd.setCursor(index,1);break;
+      case '3': if(index != 2 && index != 5){cisla[index] = 3;lcd.print(3);}index++;lcd.setCursor(index,1);break;
+      case '4': if(index != 2 && index != 5){cisla[index] = 4;lcd.print(4);}index++;lcd.setCursor(index,1);break;
+      case '5': if(index != 2 && index != 5){cisla[index] = 5;lcd.print(5);}index++;lcd.setCursor(index,1);break;
+      case '6': if(index != 2 && index != 5){cisla[index] = 6;lcd.print(6);}index++;lcd.setCursor(index,1);break;
+      case '7': if(index != 2 && index != 5){cisla[index] = 7;lcd.print(7);}index++;lcd.setCursor(index,1);break;
+      case '8': if(index != 2 && index != 5){cisla[index] = 8;lcd.print(8);}index++;lcd.setCursor(index,1);break;
+      case '9': if(index != 2 && index != 5){cisla[index] = 9;lcd.print(9);}index++;lcd.setCursor(index,1);break;
+      }
+      if (customKey =='D')
+        break;
+
+    }
+    int h = cisla[0]*10+cisla[1];
+    int m = cisla[3]*10+cisla[4];
+    int s = cisla[6]*10+cisla[7];
+    int cas = h*3600+m*60+s;
+    lcd.clear();
+    lcd.cursor_off();
+    return cas;
+}
 
 void setup() {
    Serial.begin(9600);
    delay(5000);
    clock.begin();
-   clock.armAlarm1(false);
-   clock.armAlarm2(false);
-   clock.clearAlarm1();
-   clock.clearAlarm2();
+
    clock.enable32kHz(false);
    clock.setOutput(DS3231_1HZ);
-   clock.enableOutput(true);
-   pinMode(2,INPUT_PULLUP);
-   if (clock.isOutput())
-   {
-     Serial.println("Oscilator is enabled");
-   } else
-   {
-     Serial.println("Oscilator is disabled");
-   }
-   //clock.setDateTime(2014, 4, 25, 0, 0, 0);
-   //setAlarm1(Date or Day, Hour, Minute, Second, Mode, Armed = true)
-   
+   clock.enableOutput(false);
+   pinMode(INTERRUPT_PIN,INPUT_PULLUP);
 
    Serial.println("Initialize DS3231");;
-   
+
    lcd.init();// inicializuje displej
    lcd.backlight(); // zapne podsvětlení
    lcd.print("Ahoj lidi"); // vypíše text
    alarm = false;
-   attachInterrupt(2, AlarmInterrupt, RISING);
+   attachInterrupt(INTERRUPT_PIN, AlarmInterrupt, RISING);
+   t = NastavCas();
+   clock.enableOutput(true);
 }
 
 
